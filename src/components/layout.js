63d8fc9e42bbec7a89b9663c3bfc83ebd1223b26
Ryan Vanbelkum <rvanbelkum@grubhub.com>
2019-08-07 14:17:11 -0500

    init

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import SEO from "./seo"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +26,25 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <SEO title="Home" />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          padding: `30px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
-        <main>{children}</main>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main style={{
+          padding: `4rem 0`,
+        }}>
+          {children}
+        </main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://ryanvanbelkum.me">;)</a>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Just squatting... mattrobertsonart.com has been taken.</h1>
+    <div style={{margin: `3rem 0` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>In order to get your domain back, please complete and document the following items:</h1>
+    <ul style={{padding: `0 1rem`}}>
+      <li>Rub your wife's feet for 10 minutes</li>
+      <li>Pay for the round of coffee next Monday morning</li>
+      <li>Drive to the Boathouse and buy a bean burger</li>
+      <li>Chug a gallon of milk while teaching class</li>
+      <li>Do more art stuff</li>
+    </ul>
   </Layout>
 )
 
